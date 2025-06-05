3b913dc6e05ef8d582f0e31ce6ba00d65ff45256
Kiran Bosman <kiranbosman@gmail.com>
2025-06-05 20:26:35 +0200

    Func

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -7,4 +7,95 @@ chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
 
+  let likesBefore;
+
+  test('Viewing one stock: GET request to /api/stock-prices/', function(done) {
+    chai.request(server)
+      .get('/api/stock-prices')
+      .query({ stock: 'GOOG' })
+      .end(function(err, res) {
+        assert.equal(res.status, 200);
+        assert.property(res.body, 'stockData');
+        assert.property(res.body.stockData, 'stock');
+        assert.property(res.body.stockData, 'price');
+        assert.property(res.body.stockData, 'likes');
+        assert.equal(res.body.stockData.stock, 'GOOG');
+        likesBefore = res.body.stockData.likes;
+        done();
+      });
+  });
+
+  test('Viewing one stock and liking it: GET request to /api/stock-prices/', function(done) {
+    chai.request(server)
+      .get('/api/stock-prices')
+      .query({ stock: 'GOOG', like: 'true' })
+      .end(function(err, res) {
+        assert.equal(res.status, 200);
+        assert.property(res.body, 'stockData');
+        assert.property(res.body.stockData, 'stock');
+        assert.property(res.body.stockData, 'price');
+        assert.property(res.body.stockData, 'likes');
+        assert.equal(res.body.stockData.stock, 'GOOG');
+        assert.isAtLeast(res.body.stockData.likes, likesBefore + 1);
+        done();
+      });
+  });
+
+  test('Viewing the same stock and liking it again: GET request to /api/stock-prices/', function(done) {
+    chai.request(server)
+      .get('/api/stock-prices')
+      .query({ stock: 'GOOG', like: 'true' })
+      .end(function(err, res) {
+        assert.equal(res.status, 200);
+        assert.property(res.body, 'stockData');
+        assert.property(res.body.stockData, 'stock');
+        assert.property(res.body.stockData, 'price');
+        assert.property(res.body.stockData, 'likes');
+        assert.equal(res.body.stockData.stock, 'GOOG');
+        // Likes should not increase on second like from same IP
+        assert.equal(res.body.stockData.likes, likesBefore + 1);
+        done();
+      });
+  });
+
+  test('Viewing two stocks: GET request to /api/stock-prices/', function(done) {
+    chai.request(server)
+      .get('/api/stock-prices')
+      .query({ stock: ['GOOG', 'AAPL'] })
+      .end(function(err, res) {
+        assert.equal(res.status, 200);
+        assert.property(res.body, 'stockData');
+        assert.isArray(res.body.stockData);
+        assert.lengthOf(res.body.stockData, 2);
+        const stocks = res.body.stockData.map(s => s.stock);
+        assert.includeMembers(stocks, ['GOOG', 'AAPL']);
+        res.body.stockData.forEach(stock => {
+          assert.property(stock, 'rel_likes');
+          assert.notProperty(stock, 'likes');
+          assert.property(stock, 'price');
+        });
+        done();
+      });
+  });
+
+  test('Viewing two stocks and liking them: GET request to /api/stock-prices/', function(done) {
+    chai.request(server)
+      .get('/api/stock-prices')
+      .query({ stock: ['GOOG', 'AAPL'], like: 'true' })
+      .end(function(err, res) {
+        assert.equal(res.status, 200);
+        assert.property(res.body, 'stockData');
+        assert.isArray(res.body.stockData);
+        assert.lengthOf(res.body.stockData, 2);
+        const stocks = res.body.stockData.map(s => s.stock);
+        assert.includeMembers(stocks, ['GOOG', 'AAPL']);
+        res.body.stockData.forEach(stock => {
+          assert.property(stock, 'rel_likes');
+          assert.notProperty(stock, 'likes');
+          assert.property(stock, 'price');
+        });
+        done();
+      });
+  });
+
 });
